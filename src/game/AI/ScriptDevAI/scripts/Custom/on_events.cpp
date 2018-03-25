6bcfa065582852fb1ffdd34785c99521be3a2ebe
keyshuwen <285196897@qq.com>
2018-03-25 17:27:42 +0800

    Core/Custom: Giving a custom project.

--- src/game/AI/ScriptDevAI/scripts/Custom/on_events.cpp
+++ src/game/AI/ScriptDevAI/scripts/Custom/on_events.cpp
@@ -10,10 +10,40 @@
 #include "World/World.h"
 #include <cstring>
 
-//This function is called when the player logs in (every login)  
-void OnLogin(Player* /*pPlayer*/, bool /*firstLogin*/)
-{
 
+const int ALLIANCE_TITLES[14] = {
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14
+};
+
+const int HORDE_TITLES[14] = {
+    15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28
+};
+
+const int TIME_RANKS[14] = {
+    1, 3, 6, 12, 24, 48, 72, 96, 120, 144, 168, 240, 480, 720
+};
+
+//This function is called when the player logs in (every login)  
+void OnLogin(Player* pPlayer, bool firstLogin)
+{
+    if (!pPlayer)
+        return;
+
+    if (firstLogin)
+    {
+        //新手背包
+        pPlayer->AddItem(5574, 4);
+    }
+    else
+    {
+        //军衔系统按在线时间获得(小时)
+        uint16 total_player_time = pPlayer->GetTotalPlayedTime() / HOUR;
+        for (int rank = 0; rank < 14; rank++)
+        {
+            if (total_player_time >= TIME_RANKS[rank])
+                pPlayer->SetTitle(sCharTitlesStore.LookupEntry((pPlayer->GetTeam() == ALLIANCE ? ALLIANCE_TITLES : HORDE_TITLES)[rank]));
+        }
+    }
 }
 
 //This function is called when the player logs out   
